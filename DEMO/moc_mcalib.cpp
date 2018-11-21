9536f045cd6173fbc2c71543b3b878df8110d211
abhishek balu <abhisheklokesh6008@gmail.com>
2018-11-22 01:34:50 +0530

    V1.0

---
+++ DEMO/moc_mcalib.cpp
@@ -0,0 +1,374 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mcalib.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.11.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../mcalib_V-2/mcalib.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mcalib.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.11.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MCALIB_t {
+    QByteArrayData data[56];
+    char stringdata0[1425];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MCALIB_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MCALIB_t qt_meta_stringdata_MCALIB = {
+    {
+QT_MOC_LITERAL(0, 0, 6), // "MCALIB"
+QT_MOC_LITERAL(1, 7, 23), // "on_action_NEW_triggered"
+QT_MOC_LITERAL(2, 31, 0), // ""
+QT_MOC_LITERAL(3, 32, 24), // "on_action_Open_triggered"
+QT_MOC_LITERAL(4, 57, 27), // "on_action_Save_As_triggered"
+QT_MOC_LITERAL(5, 85, 24), // "on_action_Save_triggered"
+QT_MOC_LITERAL(6, 110, 25), // "on_action_Print_triggered"
+QT_MOC_LITERAL(7, 136, 24), // "on_action_Exit_triggered"
+QT_MOC_LITERAL(8, 161, 23), // "on_action_Cut_triggered"
+QT_MOC_LITERAL(9, 185, 24), // "on_action_Copy_triggered"
+QT_MOC_LITERAL(10, 210, 25), // "on_action_Paste_triggered"
+QT_MOC_LITERAL(11, 236, 27), // "on_action_Add_Row_triggered"
+QT_MOC_LITERAL(12, 264, 30), // "on_action_Delete_Row_triggered"
+QT_MOC_LITERAL(13, 295, 26), // "on_action_Import_triggered"
+QT_MOC_LITERAL(14, 322, 26), // "on_action_To_CSV_triggered"
+QT_MOC_LITERAL(15, 349, 26), // "on_action_To_TXT_triggered"
+QT_MOC_LITERAL(16, 376, 27), // "on_action_To_XLSX_triggered"
+QT_MOC_LITERAL(17, 404, 31), // "on_action_Clear_Table_triggered"
+QT_MOC_LITERAL(18, 436, 26), // "on_action_Delete_triggered"
+QT_MOC_LITERAL(19, 463, 26), // "on_action_Locate_triggered"
+QT_MOC_LITERAL(20, 490, 27), // "on_action_Replace_triggered"
+QT_MOC_LITERAL(21, 518, 29), // "on_action_Add_Table_triggered"
+QT_MOC_LITERAL(22, 548, 32), // "on_action_Delete_Table_triggered"
+QT_MOC_LITERAL(23, 581, 30), // "on_action_Switch_Tab_triggered"
+QT_MOC_LITERAL(24, 612, 25), // "on_action_About_triggered"
+QT_MOC_LITERAL(25, 638, 33), // "on_action_About_Metrics_trigg..."
+QT_MOC_LITERAL(26, 672, 37), // "on_action_Technical_Support_t..."
+QT_MOC_LITERAL(27, 710, 27), // "on_action_Version_triggered"
+QT_MOC_LITERAL(28, 738, 38), // "on_action_System_Information_..."
+QT_MOC_LITERAL(29, 777, 28), // "on_action_Add_Page_triggered"
+QT_MOC_LITERAL(30, 806, 32), // "on_action_Main_Toolbar_triggered"
+QT_MOC_LITERAL(31, 839, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(32, 861, 10), // "Calibtable"
+QT_MOC_LITERAL(33, 872, 11), // "Calibtable1"
+QT_MOC_LITERAL(34, 884, 24), // "on_pushButton_24_clicked"
+QT_MOC_LITERAL(35, 909, 23), // "on_pushButton_5_clicked"
+QT_MOC_LITERAL(36, 933, 23), // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(37, 957, 23), // "on_pushButton_6_clicked"
+QT_MOC_LITERAL(38, 981, 24), // "on_pushButton_12_clicked"
+QT_MOC_LITERAL(39, 1006, 24), // "on_pushButton_13_clicked"
+QT_MOC_LITERAL(40, 1031, 24), // "on_pushButton_26_clicked"
+QT_MOC_LITERAL(41, 1056, 23), // "on_pushButton_8_clicked"
+QT_MOC_LITERAL(42, 1080, 23), // "on_pushButton_7_clicked"
+QT_MOC_LITERAL(43, 1104, 24), // "on_pushButton_17_clicked"
+QT_MOC_LITERAL(44, 1129, 24), // "on_pushButton_18_clicked"
+QT_MOC_LITERAL(45, 1154, 24), // "on_pushButton_10_clicked"
+QT_MOC_LITERAL(46, 1179, 23), // "on_pushButton_9_clicked"
+QT_MOC_LITERAL(47, 1203, 24), // "on_pushButton_16_clicked"
+QT_MOC_LITERAL(48, 1228, 24), // "on_pushButton_15_clicked"
+QT_MOC_LITERAL(49, 1253, 24), // "on_pushButton_14_clicked"
+QT_MOC_LITERAL(50, 1278, 24), // "on_pushButton_19_clicked"
+QT_MOC_LITERAL(51, 1303, 24), // "on_pushButton_20_clicked"
+QT_MOC_LITERAL(52, 1328, 24), // "on_pushButton_22_clicked"
+QT_MOC_LITERAL(53, 1353, 24), // "on_pushButton_23_clicked"
+QT_MOC_LITERAL(54, 1378, 21), // "on_import_c_2_clicked"
+QT_MOC_LITERAL(55, 1400, 24) // "on_pushButton_21_clicked"
+
+    },
+    "MCALIB\0on_action_NEW_triggered\0\0"
+    "on_action_Open_triggered\0"
+    "on_action_Save_As_triggered\0"
+    "on_action_Save_triggered\0"
+    "on_action_Print_triggered\0"
+    "on_action_Exit_triggered\0"
+    "on_action_Cut_triggered\0"
+    "on_action_Copy_triggered\0"
+    "on_action_Paste_triggered\0"
+    "on_action_Add_Row_triggered\0"
+    "on_action_Delete_Row_triggered\0"
+    "on_action_Import_triggered\0"
+    "on_action_To_CSV_triggered\0"
+    "on_action_To_TXT_triggered\0"
+    "on_action_To_XLSX_triggered\0"
+    "on_action_Clear_Table_triggered\0"
+    "on_action_Delete_triggered\0"
+    "on_action_Locate_triggered\0"
+    "on_action_Replace_triggered\0"
+    "on_action_Add_Table_triggered\0"
+    "on_action_Delete_Table_triggered\0"
+    "on_action_Switch_Tab_triggered\0"
+    "on_action_About_triggered\0"
+    "on_action_About_Metrics_triggered\0"
+    "on_action_Technical_Support_triggered\0"
+    "on_action_Version_triggered\0"
+    "on_action_System_Information_triggered\0"
+    "on_action_Add_Page_triggered\0"
+    "on_action_Main_Toolbar_triggered\0"
+    "on_pushButton_clicked\0Calibtable\0"
+    "Calibtable1\0on_pushButton_24_clicked\0"
+    "on_pushButton_5_clicked\0on_pushButton_3_clicked\0"
+    "on_pushButton_6_clicked\0"
+    "on_pushButton_12_clicked\0"
+    "on_pushButton_13_clicked\0"
+    "on_pushButton_26_clicked\0"
+    "on_pushButton_8_clicked\0on_pushButton_7_clicked\0"
+    "on_pushButton_17_clicked\0"
+    "on_pushButton_18_clicked\0"
+    "on_pushButton_10_clicked\0"
+    "on_pushButton_9_clicked\0"
+    "on_pushButton_16_clicked\0"
+    "on_pushButton_15_clicked\0"
+    "on_pushButton_14_clicked\0"
+    "on_pushButton_19_clicked\0"
+    "on_pushButton_20_clicked\0"
+    "on_pushButton_22_clicked\0"
+    "on_pushButton_23_clicked\0on_import_c_2_clicked\0"
+    "on_pushButton_21_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MCALIB[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      54,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  284,    2, 0x08 /* Private */,
+       3,    0,  285,    2, 0x08 /* Private */,
+       4,    0,  286,    2, 0x08 /* Private */,
+       5,    0,  287,    2, 0x08 /* Private */,
+       6,    0,  288,    2, 0x08 /* Private */,
+       7,    0,  289,    2, 0x08 /* Private */,
+       8,    0,  290,    2, 0x08 /* Private */,
+       9,    0,  291,    2, 0x08 /* Private */,
+      10,    0,  292,    2, 0x08 /* Private */,
+      11,    0,  293,    2, 0x08 /* Private */,
+      12,    0,  294,    2, 0x08 /* Private */,
+      13,    0,  295,    2, 0x08 /* Private */,
+      14,    0,  296,    2, 0x08 /* Private */,
+      15,    0,  297,    2, 0x08 /* Private */,
+      16,    0,  298,    2, 0x08 /* Private */,
+      17,    0,  299,    2, 0x08 /* Private */,
+      18,    0,  300,    2, 0x08 /* Private */,
+      19,    0,  301,    2, 0x08 /* Private */,
+      20,    0,  302,    2, 0x08 /* Private */,
+      21,    0,  303,    2, 0x08 /* Private */,
+      22,    0,  304,    2, 0x08 /* Private */,
+      23,    0,  305,    2, 0x08 /* Private */,
+      24,    0,  306,    2, 0x08 /* Private */,
+      25,    0,  307,    2, 0x08 /* Private */,
+      26,    0,  308,    2, 0x08 /* Private */,
+      27,    0,  309,    2, 0x08 /* Private */,
+      28,    0,  310,    2, 0x08 /* Private */,
+      29,    0,  311,    2, 0x08 /* Private */,
+      30,    0,  312,    2, 0x08 /* Private */,
+      31,    0,  313,    2, 0x08 /* Private */,
+      32,    0,  314,    2, 0x08 /* Private */,
+      33,    0,  315,    2, 0x08 /* Private */,
+      34,    0,  316,    2, 0x08 /* Private */,
+      35,    0,  317,    2, 0x08 /* Private */,
+      36,    0,  318,    2, 0x08 /* Private */,
+      37,    0,  319,    2, 0x08 /* Private */,
+      38,    0,  320,    2, 0x08 /* Private */,
+      39,    0,  321,    2, 0x08 /* Private */,
+      40,    0,  322,    2, 0x08 /* Private */,
+      41,    0,  323,    2, 0x08 /* Private */,
+      42,    0,  324,    2, 0x08 /* Private */,
+      43,    0,  325,    2, 0x08 /* Private */,
+      44,    0,  326,    2, 0x08 /* Private */,
+      45,    0,  327,    2, 0x08 /* Private */,
+      46,    0,  328,    2, 0x08 /* Private */,
+      47,    0,  329,    2, 0x08 /* Private */,
+      48,    0,  330,    2, 0x08 /* Private */,
+      49,    0,  331,    2, 0x08 /* Private */,
+      50,    0,  332,    2, 0x08 /* Private */,
+      51,    0,  333,    2, 0x08 /* Private */,
+      52,    0,  334,    2, 0x08 /* Private */,
+      53,    0,  335,    2, 0x08 /* Private */,
+      54,    0,  336,    2, 0x08 /* Private */,
+      55,    0,  337,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MCALIB::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MCALIB *_t = static_cast<MCALIB *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_action_NEW_triggered(); break;
+        case 1: _t->on_action_Open_triggered(); break;
+        case 2: _t->on_action_Save_As_triggered(); break;
+        case 3: _t->on_action_Save_triggered(); break;
+        case 4: _t->on_action_Print_triggered(); break;
+        case 5: _t->on_action_Exit_triggered(); break;
+        case 6: _t->on_action_Cut_triggered(); break;
+        case 7: _t->on_action_Copy_triggered(); break;
+        case 8: _t->on_action_Paste_triggered(); break;
+        case 9: _t->on_action_Add_Row_triggered(); break;
+        case 10: _t->on_action_Delete_Row_triggered(); break;
+        case 11: _t->on_action_Import_triggered(); break;
+        case 12: _t->on_action_To_CSV_triggered(); break;
+        case 13: _t->on_action_To_TXT_triggered(); break;
+        case 14: _t->on_action_To_XLSX_triggered(); break;
+        case 15: _t->on_action_Clear_Table_triggered(); break;
+        case 16: _t->on_action_Delete_triggered(); break;
+        case 17: _t->on_action_Locate_triggered(); break;
+        case 18: _t->on_action_Replace_triggered(); break;
+        case 19: _t->on_action_Add_Table_triggered(); break;
+        case 20: _t->on_action_Delete_Table_triggered(); break;
+        case 21: _t->on_action_Switch_Tab_triggered(); break;
+        case 22: _t->on_action_About_triggered(); break;
+        case 23: _t->on_action_About_Metrics_triggered(); break;
+        case 24: _t->on_action_Technical_Support_triggered(); break;
+        case 25: _t->on_action_Version_triggered(); break;
+        case 26: _t->on_action_System_Information_triggered(); break;
+        case 27: _t->on_action_Add_Page_triggered(); break;
+        case 28: _t->on_action_Main_Toolbar_triggered(); break;
+        case 29: _t->on_pushButton_clicked(); break;
+        case 30: _t->Calibtable(); break;
+        case 31: _t->Calibtable1(); break;
+        case 32: _t->on_pushButton_24_clicked(); break;
+        case 33: _t->on_pushButton_5_clicked(); break;
+        case 34: _t->on_pushButton_3_clicked(); break;
+        case 35: _t->on_pushButton_6_clicked(); break;
+        case 36: _t->on_pushButton_12_clicked(); break;
+        case 37: _t->on_pushButton_13_clicked(); break;
+        case 38: _t->on_pushButton_26_clicked(); break;
+        case 39: _t->on_pushButton_8_clicked(); break;
+        case 40: _t->on_pushButton_7_clicked(); break;
+        case 41: _t->on_pushButton_17_clicked(); break;
+        case 42: _t->on_pushButton_18_clicked(); break;
+        case 43: _t->on_pushButton_10_clicked(); break;
+        case 44: _t->on_pushButton_9_clicked(); break;
+        case 45: _t->on_pushButton_16_clicked(); break;
+        case 46: _t->on_pushButton_15_clicked(); break;
+        case 47: _t->on_pushButton_14_clicked(); break;
+        case 48: _t->on_pushButton_19_clicked(); break;
+        case 49: _t->on_pushButton_20_clicked(); break;
+        case 50: _t->on_pushButton_22_clicked(); break;
+        case 51: _t->on_pushButton_23_clicked(); break;
+        case 52: _t->on_import_c_2_clicked(); break;
+        case 53: _t->on_pushButton_21_clicked(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject MCALIB::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MCALIB.data,
+      qt_meta_data_MCALIB,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MCALIB::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MCALIB::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MCALIB.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MCALIB::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 54)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 54;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 54)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 54;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
